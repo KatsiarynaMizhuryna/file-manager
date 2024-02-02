3d81d4e896eaa0b31225190e4c24ffff8ff0a4dd
Kate <ekaterina21051988@yandex.by>
2024-02-02 12:48:12 +0200

    feat: logic for starting and finishing programm has been added

--- file-manager/src/index.js
+++ file-manager/src/index.js
@@ -1,1 +1,42 @@
+import { createInterface } from "readline";
+import { cwd } from "process";
 
+const args = process.argv.slice(2);
+const usernameArgIndex = args.indexOf("--username");
+const username = args[usernameArgIndex + 1].replace("--username=", "");
+let currentDirectory = cwd();
+
+console.log(
+  `Welcome to the File Manager, ${username}!\nYou are currently in ${currentDirectory}`
+);
+
+const rl = createInterface({
+  input: process.stdin,
+  output: process.stdout,
+  prompt: "> ",
+});
+process.on("exit", () =>
+  console.log(`Thank you for using File Manager, ${username}, goodbye!`)
+);
+process.on("SIGINT", () => process.exit(0));
+
+const promptUser = () => {
+  rl.prompt();
+  rl.on("line", (line) => {
+    const command = line.trim();
+    processCommand(command);
+    rl.prompt();
+  });
+};
+
+const processCommand = (command) => {
+  if (command === ".exit") {
+    exitHandler();
+  }
+  if (command === "pwd") {
+    console.log(`You are currently in ${currentDirectory}`);
+  }
+  console.log("Invalid input. Please enter a valid command.");
+};
+
+promptUser();
